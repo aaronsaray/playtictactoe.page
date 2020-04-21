450a9bba5eb1c440f1551349b808eb7fb6a5b6a1
Aaron Saray <me@aaronsaray.com>
2020-04-21 18:50:52 -0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <tic-tac-toe-board></tic-tac-toe-board>
+    <footer>
+      &copy; {{ new Date().getFullYear() }}
+      <a
+        href="https://morebetterfaster.io"
+        target="_blank"
+        title="For Programming With Best Practices and Bullet-proof Processes"
+      >
+        More Better Faster
+      </a>
+    </footer>
   </div>
 </template>
 
@@ -17,5 +27,23 @@ export default {
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+footer {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: #191919;
+  color: #cccccc;
+  padding: 1rem;
+  font-size: 0.9rem;
+  text-align: center;
+  a {
+    color: #cccccc;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
