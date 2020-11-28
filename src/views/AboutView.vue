7525a7634f02686846167996f8f470a7c20b1f5e
Aaron Saray <me@aaronsaray.com>
2020-11-28 15:56:34 -0600

    added about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>About page</div>
+  <div id="about">
+    <h3>About</h3>
+    <p>
+      This website is created by <a href="https://aaronsaray.com">Aaron Saray</a> and sponsored by
+      <a href="https://morebetterfaster.io">More Better Faster</a>, Aaron's company. Primarily it is
+      used as an experimental ground for trying out new technologies. But, instead of creating
+      numerous useless test websites and "todo" apps, Aaron decided to make a simple game instead.
+      This way he can experiment with new technology while letting random visitors have a little
+      fun.
+    </p>
+    <p>Now stop reading this boring thing, and <a href="/">Start a New Game</a>.</p>
+  </div>
 </template>
+
+<style lang="scss">
+#about {
+  padding: 1rem;
+  line-height: 1.5;
+  max-width: 60rem;
+  margin: auto;
+  h3 {
+    text-align: center;
+  }
+  a {
+    color: #bbccff;
+  }
+}
+</style>
